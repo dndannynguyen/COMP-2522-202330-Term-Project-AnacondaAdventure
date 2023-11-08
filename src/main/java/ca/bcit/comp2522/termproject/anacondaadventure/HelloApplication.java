4e5474d4caa2cd8f78fd5aa5a6d0a777eb091b31
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-11-08 12:13:19 -0800

    Connect to the favicon and index.css

--- src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/HelloApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        stage.getIcons().add(new javafx.scene.image.Image("file:src/main/resources/images/anaconda.jpg"));
+        stage.setTitle("Anaconda Adventure");
+        scene.getStylesheets().add(getClass().getResource("index.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
