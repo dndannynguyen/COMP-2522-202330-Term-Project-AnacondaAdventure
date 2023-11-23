80518971af224bfe5b9266797d9ac41955b33259
bf <bryan_fung@yahoo.com>
2023-11-22 16:04:15 -0800

    Implemented StartMenu layout with custom fonts and fixed-sized buttons

---
+++ src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/StartMenu.java
@@ -0,0 +1,79 @@
+package ca.bcit.comp2522.termproject.anacondaadventure;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.InputStream;
+
+public class StartMenu extends Application {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Snake Game - Start Menu");
+
+        // Load the logo image
+        Image logo = new Image(getClass().getResourceAsStream("/images/anaconda.jpg"));
+        ImageView logoView = new ImageView(logo);
+        logoView.setFitWidth(300); // Set the width of the logo image
+        logoView.setPreserveRatio(true);
+
+        // Load custom font
+        Font customFont = loadCustomFont("/fonts/Retro Gaming.ttf", 36);
+
+        Text title = new Text("Anaconda Adventure");
+        title.setFont(customFont);
+        title.setFill(Color.DARKGREEN);
+
+        Button timeAttackButton = createStyledButton("Time Attack", customFont);
+        timeAttackButton.setOnAction(event -> {
+            primaryStage.close();
+            AnacondaAdventure.setGameMode("Time Attack");
+            AnacondaAdventure.main(new String[0]);
+        });
+
+        Button endlessButton = createStyledButton("Endless", customFont);
+        endlessButton.setOnAction(event -> {
+            primaryStage.close();
+            AnacondaAdventure.setGameMode("Endless");
+            AnacondaAdventure.main(new String[0]);
+        });
+
+        VBox layout = new VBox(20); // Increased spacing between elements
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(logoView, title, timeAttackButton, endlessButton);
+        layout.setStyle("-fx-background-color: #d9ead3; -fx-padding: 50px;");
+
+        Scene scene = new Scene(layout, WIDTH, HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private Font loadCustomFont(String path, double size) {
+        InputStream fontStream = getClass().getResourceAsStream(path);
+        return Font.loadFont(fontStream, size);
+    }
+
+    private Button createStyledButton(String text, Font font) {
+        Button button = new Button(text);
+        button.setMinSize(200, 50);
+        button.setMaxSize(200, 50);
+        button.setStyle("-fx-font-family: 'Retro Gaming'; -fx-font-size: 18;");
+        return button;
+    }
+
+}
