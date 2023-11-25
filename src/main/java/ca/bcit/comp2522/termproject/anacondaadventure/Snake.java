9ef3780650b2754e39947571cc0f2c4a0795508e
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-11-24 22:39:15 -0800

    Update the Snake with Checking Collisions

--- src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Snake.java
+++ src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Snake.java
@@ -7,7 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
-    private List<Point> body;
+    private final int WIDTH = AnacondaAdventure.getWIDTH();
+    private final int HEIGHT = AnacondaAdventure.getHEIGHT();
+
+    private static List<Point> body;
     private Direction direction;
     private static final Color SNAKE_COLOR = Color.GREEN;
 
@@ -16,16 +19,28 @@ public class Snake {
         body.add(new Point(5, 5));
         body.add(new Point(5, 6));
         body.add(new Point(5, 7));
+        body.add(new Point(5, 8));
+        body.add(new Point(5, 9));
+        body.add(new Point(5, 10));
+        body.add(new Point(5, 11));
+        body.add(new Point(5, 12));
+        body.add(new Point(5, 13));
         direction = Direction.RIGHT;
     }
+
+    public static int getBodySize() {
+        return body.size();
+    }
+
 
     public void init() {
 
     }
 
     public void update() {
-        move();
-
+        if (!checkCollisionWithBoundaries(WIDTH, HEIGHT) && !checkCollisionWithSelf()) {
+            move();
+        }
     }
 
     private void move() {
@@ -44,12 +59,25 @@ public class Snake {
     }
 
     private boolean isWithinBoundaries(int x, int y, int width, int height) {
-        return x >= 0 && y >= 0 && x < width && y < height;
+        return x >= 0 && y >= 0 && x <= width && y <= height;
     }
 
     public boolean checkCollisionWithBoundaries(int width, int height) {
         Point head = body.get(0);
-        return !isWithinBoundaries(head.getX(), head.getY(), width, height);
+        int x = head.getX();
+        int y = head.getY();
+        return !isWithinBoundaries(x, y, width, height);
+    }
+
+    public boolean checkCollisionWithSelf() {
+        Point head = body.get(0);
+        for (int i = 1; i < body.size(); i++) {
+            Point segment = body.get(i);
+            if (head.getX() == segment.getX() && head.getY() == segment.getY()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void render(GraphicsContext gc, int tileSize) {
@@ -58,6 +86,10 @@ public class Snake {
             gc.fillRect(segment.getX() * tileSize, segment.getY() * tileSize, tileSize, tileSize);
         }
     }
+
+
+
+
 
 
 }
