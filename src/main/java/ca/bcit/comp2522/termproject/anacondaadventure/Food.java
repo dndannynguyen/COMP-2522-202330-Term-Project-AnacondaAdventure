6beb1808ce304e5ae0c414b6d6b919b569e8af39
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-12-05 01:57:11 -0800

    Update food for snake

--- src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Food.java
+++ src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Food.java
@@ -2,12 +2,14 @@ package ca.bcit.comp2522.termproject.anacondaadventure;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+
+import java.util.Objects;
 import java.util.Random;
 
 public class Food {
-    private static final Color COLOR_NORMAL = Color.ROSYBROWN;
+    private static final Color COLOR_NORMAL = Color.RED;
     private static final Color COLOR_POWER_UP = Color.GOLD;
-    private static final Color COLOR_NERF = Color.DARKSLATEGRAY;
+    private static final Color COLOR_NERF = Color.DARKGRAY;
 
     private Point point;
     private FoodType type;
@@ -40,11 +42,20 @@ public class Food {
         gc.fillRect(point.getX() * tileSize, point.getY() * tileSize, tileSize, tileSize);
     }
 
-    public static Food generateRandomFood(Point minPoint, Point maxPoint) {
+    public static Food generateRandomFood(Point minPoint, Point maxPoint, Obstacle obstacle, Snake snake, String mode) {
         Random random = new Random();
-        int x = random.nextInt(maxPoint.getX() - minPoint.getX()) + minPoint.getX();
-        int y = random.nextInt(maxPoint.getY() - minPoint.getY()) + minPoint.getY();
-
+        int x, y;
+        for (;;){
+            x = random.nextInt(maxPoint.getX() - minPoint.getX()) + minPoint.getX();
+            y = random.nextInt(maxPoint.getY() - minPoint.getY()) + minPoint.getY();
+            if(obstacle == null)
+                break;
+            if(!checkForObstacles(x, y, obstacle) && !checkForSnake(x, y, snake))
+                break;
+        }
+        if(Objects.equals(mode, "Endless")){
+            return new Food(new Point(x, y), FoodType.NORMAL);
+        }
 
         FoodType type = FoodType.NORMAL;
         int foodTypeRandom = random.nextInt(10);
@@ -57,7 +68,24 @@ public class Food {
         return new Food(new Point(x, y), type);
     }
 
-    public void init() {
+    public static boolean checkForObstacles(int x, int y, Obstacle obstacle){
+        for(int i = 0; i < obstacle.getObstacles().size(); i++){
+            Point obs = obstacle.getObstacles().get(i);
+            if(obs.getX() == x && obs.getY() == y)
+                return true;
+        }
+        return false;
+    }
+    public static boolean checkForSnake(int x, int y, Snake snake){
+        for(int i = 0; i < snake.getSize(); i++){
+            Point obs = snake.getBody().get(i);
+            if(obs.getX() == x && obs.getY() == y)
+                return true;
+        }
+        return false;
+    }
 
+    public FoodType getType(){
+        return type;
     }
 }
