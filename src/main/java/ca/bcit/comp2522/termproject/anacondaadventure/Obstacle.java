c737294b08df054501d4cde73aab3daff81212fa
dndannynguyen <dn.dannynguyen@hotmail.com>
2023-12-05 02:03:04 -0800

    Create Obstacles

--- src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Obstacle.java
+++ src/main/java/ca/bcit/comp2522/termproject/anacondaadventure/Obstacle.java
@@ -1,20 +1,40 @@
 package ca.bcit.comp2522.termproject.anacondaadventure;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Obstacle {
 
+    private Color OBSTACLE_COLOR = Color.BLUE;
+    private static List<Point> obstacles = new ArrayList<>();
 
-    public void spawn() {
-
+    public Obstacle(){
+        obstacles = new ArrayList<>();
     }
 
-    public void render(GraphicsContext gc) {
-
+    public void spawn(Point minPoint, Point maxPoint) {
+        Random random = new Random();
+        int x, y;
+        for (;;){
+            x = random.nextInt(maxPoint.getX() - minPoint.getX()) + minPoint.getX();
+            y = random.nextInt(maxPoint.getY() - minPoint.getY()) + minPoint.getY();
+            if(x != 0 && y!= 2)
+                break;
+        }
+        obstacles.add(new Point(x,y));
     }
 
-    public void init() {
-
+    public void render(GraphicsContext gc, int tileSize) {
+        gc.setFill(OBSTACLE_COLOR);
+        for (Point segment : obstacles) {
+            gc.fillRect(segment.getX() * tileSize, segment.getY() * tileSize, tileSize, tileSize);
+        }
+    }
+    public List<Point> getObstacles(){
+        return obstacles;
     }
 }
-
